c611dc1d6163405a21170e7116a92f58f598fd67
Zircoz <dixit.setu@gmail.com>
2018-10-26 17:45:36 +0530

    .

---
+++ studentPlusinstructor/routes/studentRoutes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/my-account', function(req, res) {
+  res.render('myaccountStudent');
+});
+
+module.exports = router;
