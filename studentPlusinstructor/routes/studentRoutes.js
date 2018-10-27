15110c1887a478072d2dcb1010be335bd508d032
Zircoz <dixit.setu@gmail.com>
2018-10-27 21:29:16 +0530

    -added register for instructor

--- studentPlusinstructor/routes/studentRoutes.js
+++ studentPlusinstructor/routes/studentRoutes.js
@@ -5,6 +5,9 @@ router.get('/my-account', function(req, res) {
   res.render('myaccountStudent');
 });
 
+router.get('/registerInstructor', function(req, res) {
+  res.render('registerInstructor');
+});
 
 
 module.exports = router;
